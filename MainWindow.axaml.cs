50ac5c7b9266e4c4f8ab32a3a881be020f1ac98f
Juff-Ma <julianrossbach3012@gmail.com>
2022-07-11 18:00:08 +0200

    Projektdateien hinzufügen.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PW_Generator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
